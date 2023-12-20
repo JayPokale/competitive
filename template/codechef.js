98e6bf41e7542b48259accfe329ed03628dc7e41
Jay Pokale <jay.pokale.35@gmail.com>
2023-12-20 11:11:06 +0530

    Codechef template added

---
+++ template/codechef.js
@@ -0,0 +1,28 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString.trim().split("\n");
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  var t = parseInt(readLine());
+
+  while (t--) {
+    // Your Logic goes here
+  }
+}
